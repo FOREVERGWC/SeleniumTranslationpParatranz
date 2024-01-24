c649dab6aa517806446ec5af8e790dcdabdf5798
0408葛瑶 <916586595@qq.com>
2024-01-24 09:44:04 +0800

    完善异常处理

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 
 import requests
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 project_id = 9158  # 云平台项目ID
@@ -15,24 +16,46 @@ def translation_words(words):
     for word in words:
         path = f"https://www.bing.com/translator?ref=TThis&text={word['original'].replace(' ', '%20')}&from=auto&to={to}"
         browser.get(path)
-        time.sleep(5)  # 不确认多少秒，五秒比较保守，短了页面可能出不来
-        translation = browser.find_element(By.ID, "tta_output_ta").get_attribute("value")
-        word["translation"] = translation
-        # TODO 执行更新平台
+        # TODO 判断文本长度，超出指定长度进行跳过，不执行翻译
+        for i in range(1, 200):
+            try:
+                element = browser.find_element(By.ID, "tta_output_ta")
+            except NoSuchElementException:
+                if i == 199:
+                    print(f"跳过词条：{word['original']}")
+                    break
+                print(f"第{i}次抛出异常，长度：{len(path)}")
+                time.sleep(1)
+                continue
+            else:
+                translation = element.get_attribute("value")
+                for j in range(1, 200):
+                    if translation == " ...":
+                        time.sleep(1)
+                        translation = browser.find_element(By.ID, "tta_output_ta").get_attribute("value")
+                        continue
+                    break
+                word["translation"] = translation
+                update_word(word)
+                break
     browser.close()
     return words
 
 
+def update_word(word):
+    if word["translation"] == '':
+        return
+    url = f"https://paratranz.cn/api/projects/{project_id}/strings/{word['id']}"
+    headers = {"Authorization": authorization, "Content-Type": "application/json", "Connection": "close"}
+    data = json.dumps({"translation": word["translation"], "stage": 1, "uid": None})
+    response = requests.put(url, headers=headers, data=data)
+    # TODO 判断响应状态码，200则成功；429则请求过多，获取响应头Retry-After信息并等待相应时长重新执行
+    print(f"已更新词条：{word['original']}", response)
+
+
 def update_words(words):
     for word in words:
-        if word["translation"] == '':
-            continue
-        url = f"https://paratranz.cn/api/projects/{project_id}/strings/{word['id']}"
-        headers = {"Authorization": authorization, "Content-Type": "application/json", "Connection": "close"}
-        data = json.dumps({"translation": word["translation"], "stage": 1, "uid": None})
-        response = requests.put(url, headers=headers, data=data)
-        # TODO 判断响应状态码，200则成功；429则请求过多，获取响应头Retry-After信息并等待相应时长重新执行
-        print(f"已更新词条：{word['original']}", response)
+        update_word(word)
 
 
 if __name__ == '__main__':
@@ -44,6 +67,6 @@ if __name__ == '__main__':
         data = response.json()
         if status_code == 200:
             words = translation_words(data["results"])
-            update_words(words)
+            # update_words(words)
         if page == data["pageCount"]:
             break
